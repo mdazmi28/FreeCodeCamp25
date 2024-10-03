01075dd252da7e0fa08d2e66a45ab81d1b354194
mdazmi28 <md.azmi.siddique@gmail.com>
2024-10-03 18:15:06 +0600

    Minor Chnage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 
 // import Accor from "./Accordion/Accor";
-import ColorGenerate from "./ColorGenerate/ColorGenerate";
+// import ColorGenerate from "./ColorGenerate/ColorGenerate";
 import StarReview from "./StarReview/StarReview";
 
 // import accordion from "./Accordion/accordion";
--- src/StarReview/StarReview.jsx
+++ src/StarReview/StarReview.jsx
@@ -1,8 +1,33 @@
 // import React from 'react';
+import { useState } from "react";
+import { CiStar } from "react-icons/ci";
 
 const StarReview = () => {
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
+
+    const handleClick =(getCurrentID) =>{
+
+    }
+
+    const handleOnMouseEnter = (getCurrentID) =>{
+
+    }
+
+    const handleOnMouseLeave = (getCurrentID) =>[
+
+    ]
+    const stars = Array.from({ length: 10 });
     return (
-        <div className="min-h-screen w-full flex flex-col items-center justify-center">
+        <div className="min-h-screen w-full flex items-center justify-center">
+            {
+                stars.map((_, index) => (
+                        // index = index +1;
+                        <CiStar key={index} className="h-[40px] w-[40px]" onClick={handleClick(index+1)} onMouseMove={handleOnMouseEnter(index+1)} onMouseLeave={handleOnMouseLeave(index+1)}/>
+                  
+                    
+                ))
+            }
             
         </div>
     );
